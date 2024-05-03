c776c4cd7f57cea52016109e7e2ff33f0ec2fb6e
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2024-05-03 11:23:04 +0200

    Création du CRUD Catégorie et insertion des différentes catégories

--- src/Controller/Admin/CategoriesCrudController.php
+++ src/Controller/Admin/CategoriesCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Categories;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CategoriesCrudController extends AbstractCrudController
 {
@@ -15,14 +17,32 @@ class CategoriesCrudController extends AbstractCrudController
         return Categories::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('title', 'Nom')->onlyOnIndex(),
+            TextField::new('title', 'Nom')->onlyWhenCreating(),
+            AssociationField::new('products')->onlyWhenUpdating(),
+            AssociationField::new('products')->onlyOnIndex(),
+            TextEditorField::new('description'),
+            ImageField::new('image')->setUploadDir('assets/img/categories/')->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')->onlyWhenCreating(),
+            ImageField::new('image')->setUploadDir('assets/img/categories/')->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')->onlyWhenUpdating()->setFormTypeOptions([
+                'required' => false,
+            ]),
+            ImageField::new('image')->setBasePath('img/categories/')->hideOnForm(),
             TextEditorField::new('description'),
         ];
     }
-    */
+    
+
+
+    public function createEntity(string $entityFqcn)
+    {
+        $produit = new $entityFqcn;
+        $produit->setCreatedAt(new \DateTimeImmutable());
+        $produit->setUpdatedAt(new \DateTimeImmutable());
+        return $produit;
+    }
 }

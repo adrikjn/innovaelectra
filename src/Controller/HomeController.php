e6adc261841cdea4fd9d68fe814bc75caf93148a
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2024-05-13 17:29:20 +0200

    Création du controller basket et son twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -61,4 +61,10 @@ class HomeController extends AbstractController
         ]);
     }
 
+    #[Route('/basket}', name: 'basket.details')]
+    public function getBasket(): Response
+    {
+        return $this->render('home/basket.html.twig');
+    }
+
 }

abf0ad95ff212a73068757e702072e6cd55fdcdf
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2024-05-16 17:01:11 +0200

    x

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,5 @@
 import "./bootstrap.js";
 
-document.getElementById('toggleButton').addEventListener('click', function() {
-    this.style.display = 'none';
-    document.getElementById('descriptionText').classList.remove('d-none');
-});
 
 
 import "./styles/app.css";

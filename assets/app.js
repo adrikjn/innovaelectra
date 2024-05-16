506f484b6397466617381f1616c7ba568f7fab71
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2024-05-16 18:50:06 +0200

    Système panier mais need Reload de la page..

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,43 @@
 import "./bootstrap.js";
 
+document.addEventListener('DOMContentLoaded', () => {
 
+    const setupAddToCart = () => {
+        const addToCartButton = document.getElementById('addToCartButton');
+        const successMessage = document.getElementById('successMessage');
+
+        if (addToCartButton && successMessage) {
+            addToCartButton.addEventListener('click', () => {
+                const product = {
+                    id: addToCartButton.dataset.productId,
+                    title: addToCartButton.dataset.productTitle,
+                    price: parseFloat(addToCartButton.dataset.productPrice),
+                    image: addToCartButton.dataset.productImage,
+                    quantity: 1
+                };
+
+                let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+                const productIndex = cart.findIndex(item => item.id === product.id);
+
+                if (productIndex === -1) {
+                    cart.push(product);
+                } else {
+                    cart[productIndex].quantity += 1;
+                }
+
+                localStorage.setItem('cart', JSON.stringify(cart));
+
+                // Affichage du message de succès
+                successMessage.classList.remove('d-none');
+
+            });
+        }
+    };
+
+    // Initialize all functions
+    setupAddToCart();
+});
 
 import "./styles/app.css";
 
-console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉");
